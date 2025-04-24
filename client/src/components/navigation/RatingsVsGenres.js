38a0ef0b1136d8409e3917a238bd3ddee7cbea72
rp222nc <rp222nc@student.lnu.se>
2025-04-24 23:40:54 +0200

    Replace plotterChart with heatmap

--- client/src/components/navigation/RatingsVsGenres.js
+++ client/src/components/navigation/RatingsVsGenres.js
@@ -10,28 +10,33 @@ import YearPicker from "../common/YearPicker.js"
  */
 const RatingsVsGenre = () => {
     const [plotterData, setPlotterData] = useState(null)
+    const [ratingObject, setRatingObject] = useState(null)
     const [loading, setLoading] = useState(false)
-    const [year, setYear] = useState(1985)
+    const [year, setYear] = useState(2000)
 
     useEffect(() => {
         const createPlotterData = async () => {
             try {
                 setLoading(true)
                 const ratingObject = await fetchRating()
+                setRatingObject(ratingObject)
+                
                 const genreData = await fetchGenres(year)
-                console.log('Rating Object:', ratingObject)
 
-                const plotterData = genreData.games.flatMap(game => 
+                const rawPlotterData = genreData.games.flatMap(game => 
                     game.genres.map(genre => {
-                        const ratingKey = Object.keys(ratingObject).find(key => ratingObject[key] === game.rating)
+                        const ratingKey = Object.keys(ratingObject).find(key => ratingObject[key] === game.rating?.text)
                         return {
                             genre,
-                            rating: ratingKey || Object.keys(ratingObject).length, // Fallback - Last rating = Rating pending
+                            rating: parseInt(ratingKey) || Object.keys(ratingObject).length, // Fallback - Last rating = Rating pending
                         }
                     })
                 )
 
-                setPlotterData(plotterData)
+                // Calculate frequencies
+                const processedPlotterData = processData(rawPlotterData)
+
+                setPlotterData(processedPlotterData)
                 setLoading(false)
             }
             catch (error) {
@@ -61,12 +66,30 @@ const RatingsVsGenre = () => {
         return ratingObject
     }
 
+    const processData = (data) => {
+        const frequencies = {}
+        data.forEach(item => {
+            const key = `${item.genre.name}-${item.rating}`
+            frequencies[key] = (frequencies[key] || 0) + 1
+        })
+
+        const processedData = Object.entries(frequencies).map(([key, value]) => {
+            const [genre, rating] = key.split('-')
+            return {
+                genre: genre,
+                rating: parseInt(rating),
+                frequency: value
+            }
+        })
+        return processedData
+    }
+
     return (
         <div>
             <div className="year-picker" style={{ padding: '10px' }}>
                 <YearPicker updateYear={setYear} />
             </div>
-            {plotterData && <PlotterChart data={plotterData} />}
+            {plotterData && ratingObject && <PlotterChart data={plotterData} ratings={ratingObject}/>}
             {loading && <Loader blur={true} />}
         </div>
     )
--- client/src/components/visuals/PlotterChart.js
+++ client/src/components/visuals/PlotterChart.js
@@ -1,48 +1,82 @@
 import { useEffect, useState, useRef } from "react"
 import * as echarts from 'echarts'
-import SlideSwitch from "../common/SlideSwitch.js"
 
 /**
  * Displays a PlotterChart component.
  */
-const PlotterChart = ({ data }) => {
+const PlotterChart = ({ data, ratings }) => {
     const chartRef = useRef(null)
+    const dataLength = data.length
     console.log('PlotterChart data:', data)
+    console.log('Math max:', Math.max(...data.map(item => item.frequency)))
 
-    const option = {
-        title: {
-            text: 'Genre vs Rating',
-            left: 'center'
-        },
-        xAxis: {
-            type: 'category',
-            data: [...new Set(data.map(item => item.genre))],
-        },
-        yAxis: {
-            type: 'value',
-            min: 1,
-            max: 6,
-            interval: 1,
-            data: [...new Set(data.map(item => item.rating))],
-            axisLabel: {
-                formatter: value => {
-                    const labels = ['Rating 1', 'Rating 2', 'Rating 3', 'Rating 4', 'Rating 5', 'Rating 6']
-            }
-        },
-        series: [
-          {
-            symbolSize: 20,
-            data: [],
-            type: 'scatter'
-          }
-        ]
-      }
+    useEffect(() => {
+        setChartOptions()
+    }, [data])
+
+    const setChartOptions = () => {
+        const chart = echarts.init(chartRef.current)
+        const option = {
+            title: {
+                text: 'Genre vs Rating',
+                left: 'center'
+            },
+            tooltip: {
+                position: 'top'
+            },
+            grid: {
+                height: '50%',
+                top: '10%'
+            },
+            xAxis: {
+                type: 'category',
+                data: [...new Set(data.map(item => item.genre))],
+                splitArea: {
+                    show: true
+                }
+            },
+            yAxis: {
+                type: 'category',
+                data: [...new Set(data.map(item => item.rating))],
+                axisLabel: {
+                    formatter: value => {
+                        return ratings[value] || `Rating ${value}`
+                    }
+                },
+                splitArea: {
+                    show: true
+                  }
+            },
+            visualMap: {
+                min: 1,
+                max: Math.max(...data.map(item => item.frequency)),
+                calculable: true,
+                orient: 'horizontal',
+                left: 'center',
+                top: 'center',
+                inRange: {
+                    color: ['#f6d8a7', '#ff0000'],
+                },
+            },
+            series: [
+                {
+                    data: data.map(item => [item.genre.name, item.rating, item.frequency]), // Adding a small random offset to the y value
+                    type: 'heatmap',
+                    label: {
+                        show: true,
+                        formatter: '{@[2]}',
+                    }
+                }
+            ]
+        }
+        chart.setOption(option)
     }
 
 
     return (
         <div className="cc0-view-sound">
             <h1>Genre vs Rating Plotter Chart</h1>
+            <h2>Number of games: {dataLength}</h2>
             <div ref={chartRef} style={{ width: '100%', height: '400px' }}></div>
         </div>
     )
