b6573bd66cf285549c2e79324ad26609e4000d6a
rp222nc <rp222nc@student.lnu.se>
2025-04-25 16:58:02 +0200

    Add comments

--- src/services/FetchService.js
+++ src/services/FetchService.js
@@ -6,6 +6,13 @@ import { gql, request } from 'graphql-request'
 
 // TODO Create a class
 
+/**
+ * Fetches data from the GraphQL API.
+ *  
+ * @param {string} query - The GraphQL query to execute.
+ * @return {Promise<Object>} - The response from the API.
+ * @throws {Error} - If the request fails.
+ */
 const graphqlFetch = async (query) => {
   try {
     const response = await request(process.env.REACT_APP_BACKEND_URL, query)
@@ -16,6 +23,12 @@ const graphqlFetch = async (query) => {
   }
 }
 
+/**
+ * Fetches all games from the API.
+ * 
+ * @returns {Promise<Object>} - The response from the API.
+ * @throws {Error} - If the request fails.
+ */
 export const getAllGames = async () => {
   const query = gql`
     query {
@@ -28,6 +41,12 @@ export const getAllGames = async () => {
   return await graphqlFetch(query)
 }
 
+/**
+ * Fetches all genres from the API.
+ * 
+ * @param {number} year 
+ * @returns 
+ */
 export const getGameGenres = async (year) => {
   const query = gql`
     query {
@@ -43,6 +62,15 @@ export const getGameGenres = async (year) => {
   return await graphqlFetch(query)
 }
 
+/**
+ * Fetches games by year and genre from the API.
+ *
+ * @param {number} year 
+ * @param {String} genre 
+ * @param {number} limit 
+ * @param {number} page 
+ * @returns 
+ */
 export const getGamesByYearAndGenre = async (year, genre, limit, page) => {
   const query = gql`
     query {
@@ -60,6 +88,12 @@ export const getGamesByYearAndGenre = async (year, genre, limit, page) => {
   return await graphqlFetch(query)
 }
 
+/**
+ * Fetches platforms from the API.
+ *
+ * @param {number} year 
+ * @returns 
+ */
 export const getPlatforms = async (year) => {
   const query = gql`
     query {
@@ -73,6 +107,12 @@ export const getPlatforms = async (year) => {
   return await graphqlFetch(query)
 }
 
+/**
+ * Fetches genres and ratings from the API.
+ *
+ * @param {number} year 
+ * @returns 
+ */
 export const getGenresAndRatings = async (year) => {
   const query = gql`
     query {
@@ -90,6 +130,11 @@ export const getGenresAndRatings = async (year) => {
   return await graphqlFetch(query)
 }
 
+/**
+ * Fetches ratings from the API.
+ *
+ * @returns {Promise<Object>} - The response from the API.
+ */
 export const getRatings = async () => {
   const query = gql`
         query {
