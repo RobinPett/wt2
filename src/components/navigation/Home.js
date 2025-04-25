efff296d91b2a880599ebde18dfad2df8e60e35e
rp222nc <rp222nc@student.lnu.se>
2025-04-25 16:21:16 +0200

    Update homepage

--- src/components/navigation/Home.js
+++ src/components/navigation/Home.js
@@ -7,7 +7,44 @@ const Home = () => {
   return (
     <div className="home">
       <main>
-        <h1>Welcome to the Game Visualizer</h1>
+        <div className="home-hero">
+          <div className="hero-text-container">
+            <h1 className='hero-text'>Game</h1>
+            <h1 className='hero-text'>Visualizer</h1>
+          </div>
+          <h1><b>Welcome to the Game Visualizer!</b></h1>
+          <p className="home-text">
+            This project aims to give an overview of popular genres and platforms for video games between <b>1970 - 2022.</b>
+            <br />
+            The dataset stores over 400,000 games with the following data:
+          </p>
+          <ul className='dataset-attributes'>
+            <li>Title</li>
+            <li>Release year</li>
+            <li>Developers</li>
+            <li>Genres</li>
+            <li>Platforms</li>
+            <li>Rating</li>
+          </ul>
+          <p className="home-text">
+            With the dataset I can visualize how each year has impacted the types of games on the market.
+          </p>
+          <ul className='dataset-attributes'>
+            <li>Which genres were popular?</li>
+            <li>Which platforms has dominated?</li>
+            <li>How does genres and esrb-ratings correlate?</li>
+          </ul>
+
+          <p className="home-text">
+            The webapplication visualizes this information with:
+          </p>
+
+          <ul className='dataset-attributes'>
+            <li>Bar graph - Popular genres</li>
+            <li>Pie chart - Dominant platforms</li>
+            <li>Heatmap - Frequency of genres and ratings</li>
+          </ul>
+        </div>
       </main>
     </div>
   )
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -7,7 +7,7 @@ import { NavLink } from 'react-router-dom'
  */
 const Navbar = () => {
   return (
-    <nav className="cc0-navbar">
+    <nav className="navbar">
       <NavLink to='/' className="text-center text-red-600 font-bold">Game Visualizer</NavLink>
       <div className="links">
         <NavLink exact activeClassName='cc0-link-active' className='cc0-link' to='/genres'>Genres</NavLink>
